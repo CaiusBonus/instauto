4756b9e28204076d8ac1370cb97435ac1771c48c
Maroš <maros.gonda@student.tuke.sk>
2020-12-01 19:23:40 +0100

    + edition of test

--- instauto/api/actions/structs/tests/test_search.py
+++ instauto/api/actions/structs/tests/test_search.py
@@ -2,7 +2,6 @@ import unittest
 
 from instauto.api.client import ApiClient
 import instauto.api.actions.structs.search as sr
-from instauto.api.actions.tests.common import helper
 
 
 class TestSearch(unittest.TestCase):
@@ -11,10 +10,8 @@ class TestSearch(unittest.TestCase):
         client = ApiClient(testing=True)
         s = sr.Tag('test',1)
         s.fill(client)
-        helper(s, self)
 
     def test_username(self):
         client = ApiClient(testing=True)
         s = sr.Username("test",1)
         s.fill(client)
-        helper(s, self)
